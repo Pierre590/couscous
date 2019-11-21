f33091ea14b1ab0206c11b7d21ba97df65ebfaa5
pierre <nanashi4781@gmail.com>
2019-11-21 16:01:38 +0100

    finsansgras

--- couscous.js
+++ couscous.js
@@ -1,7 +1,7 @@
-/*$(function() {
+$(function() {
     const items = $('.nav-item');
     items.on('click', function() {
         items.removeClass('active');
         $(this).addClass('active');
     })
-})*/
+})
