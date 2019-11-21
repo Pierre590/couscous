a286fd480900d97fbb64218aa1cfbbee5ce3616f
pierre <nanashi4781@gmail.com>
2019-11-21 14:35:00 +0100

    finishsansjs

--- couscous.js
+++ couscous.js
@@ -1,7 +1,7 @@
 /*$(function() {
     const items = $('.nav-item');
     items.on('click', function() {
-        items.removeClass('.active');
-        $(this).addClass('.active');
+        items.removeClass('active');
+        $(this).addClass('active');
     })
 })*/
